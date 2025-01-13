e2f8079c7f2629375e7fe8976293febbd867a0d1
AnshikaPand <anshikapandey950@gmail.com>
2025-01-13 21:49:59 +0530

    Initial commit

---
+++ Script.js
@@ -0,0 +1,47 @@
+// function addStrings(str1, str2) {
+//     let result = '';
+//     let i = 0;
+
+    
+//     for (i = 0; str1[i] !== undefined; i++) {
+//         result += str1[i];
+//     }
+
+    
+    
+//     for (i = 0; str2[i] !== undefined; i++) {
+//         result += str2[i];
+//     }
+
+//     return result;
+// }
+
+
+// let str1 = "Hello";
+// let str2 = "World";
+// let result = addStrings(str1, str2);
+// console.log(result); 
+
+
+// let str = ""; 
+
+
+// let isEmpty = true;
+
+// for (let i = 0; ; i++) {
+//     try {
+        
+//         str[i];
+        
+//         isEmpty = false;
+//         break; 
+//     } catch (e) {
+       
+//         break; 
+        
+//     }
+// }
+
+// console.log("The string is empty:");
+
+// let A = "10"
